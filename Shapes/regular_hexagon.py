ea5f27cc83f2324021f3ae21648c9287b027b6bf
eli <lykkh78@gmail.com>
2025-06-25 10:01:19 +0300

    adding regular hexagon class

---
+++ Shapes/regular_hexagon.py
@@ -0,0 +1,12 @@
+from Shapes.square import Square
+from math import *
+
+
+class RegularHexagon(Square):
+    def __init__(self, side):
+        super().__init__(side)
+    def get_area(self):
+        return ((3 * sqrt(3))/2) * self._len_sid**2
+    def get_perimeter(self):
+        return super().get_perimeter() + self._len_sid * 2
+
